1bf95ea47649e449553a442cb65f459da56ce801
Ilia Vlasov <elijah.vlasov@gmail.com>
2024-07-26 12:34:09 +0100

    Add composite verifier

--- latticefold/src/nifs.rs
+++ latticefold/src/nifs.rs
@@ -6,11 +6,11 @@ use std::marker::PhantomData;
 
 use ark_crypto_primitives::sponge::Absorb;
 use ark_ff::Field;
-use decomposition::{DecompositionError, DecompositionProver};
-use folding::{FoldingError, FoldingProver};
+use decomposition::{DecompositionError, DecompositionProver, DecompositionVerifier};
+use folding::{FoldingError, FoldingProver, FoldingVerifier};
 use lattirust_arithmetic::challenge_set::latticefold_challenge_set::OverField;
 use lattirust_arithmetic::ring::Ring;
-use linearization::{LinearizationError, LinearizationProver};
+use linearization::{LinearizationError, LinearizationProver, LinearizationVerifier};
 use thiserror::Error;
 
 use crate::arith::{Witness, LCCCS};
@@ -109,5 +109,49 @@ impl<F: Field + Absorb, R: OverField<F>, T: Transcript<F, R>> NIFSProver<F, R, T
         transcript: &mut impl Transcript<F, R>,
     ) -> Result<(LCCCS<R>, Witness<R>, LatticefoldProof<F, R, T>), LatticefoldError<R>> {
         Self::prove_aux(acc, w_acc, cm_i, w_i, transcript)
+    }
+}
+
+impl<F: Field + Absorb, R: OverField<F>, T: Transcript<F, R>> NIFSVerifier<F, R, T> {
+    fn verify_aux<
+        L: LinearizationVerifier<F, R, T>,
+        D: DecompositionVerifier<F, R, T>,
+        FV: FoldingVerifier<F, R, T>,
+        E: From<L::Error> + From<D::Error> + From<FV::Error>,
+    >(
+        acc: &LCCCS<R>,
+        cm_i: &CCCS<R>,
+        proof: &ComposedProof<F, R, T, L::Prover, D::Prover, FV::Prover>,
+        transcript: &mut impl Transcript<F, R>,
+    ) -> Result<LCCCS<R>, E> {
+        let linearized_cm_i = L::verify(cm_i, &proof.linearization_proof, transcript)?;
+        let decomposed_acc = D::verify(acc, &proof.decomposition_proof_l, transcript)?;
+        let decomposed_cm_i =
+            D::verify(&linearized_cm_i, &proof.decomposition_proof_r, transcript)?;
+
+        let lcccs_s = {
+            let mut decomposed_acc = decomposed_acc;
+            let mut decomposed_cm_i = decomposed_cm_i;
+
+            decomposed_acc.append(&mut decomposed_cm_i);
+
+            decomposed_acc
+        };
+
+        Ok(FV::verify(&lcccs_s, &proof.folding_proof, transcript)?)
+    }
+
+    pub fn verify(
+        acc: &LCCCS<R>,
+        cm_i: &CCCS<R>,
+        proof: &LatticefoldProof<F, R, T>,
+        transcript: &mut impl Transcript<F, R>,
+    ) -> Result<LCCCS<R>, LatticefoldError<R>> {
+        Self::verify_aux::<
+            NIFSVerifier<F, R, T>,
+            NIFSVerifier<F, R, T>,
+            NIFSVerifier<F, R, T>,
+            LatticefoldError<R>,
+        >(acc, cm_i, proof, transcript)
     }
 }
