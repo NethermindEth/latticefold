e9f57bec51525ca2ef76f249aa5413891a00e134
Alex Klein <matthewklein345@gmail.com>
2024-08-05 17:45:49 +0200

    Fix mle utils

--- latticefold/src/utils/mle.rs
+++ latticefold/src/utils/mle.rs
@@ -8,15 +8,17 @@ use lattirust_arithmetic::ring::Ring;
 /// Pad matrix so that its columns and rows are powers of two
 pub fn pad_matrix<R: Ring>(m: &SparseMatrix<R>) -> SparseMatrix<R> {
     let nrows_new = m.nrows().next_power_of_two();
+
     let ncols_new = m.ncols().next_power_of_two();
 
     if m.nrows() == nrows_new && m.ncols() == ncols_new {
         m.clone()
     } else {
+        let offsets = pad_vector(m.col_offsets().to_vec(), ncols_new + 1);
         SparseMatrix::try_from_csc_data(
             nrows_new,
             ncols_new,
-            m.col_offsets().to_vec(),
+            offsets,
             m.row_indices().to_vec(),
             m.values().to_vec(),
         )
@@ -65,12 +67,12 @@ pub fn vec_to_dense_mle<R: Ring>(n_vars: usize, v: &[R]) -> DenseMultilinearExte
 pub fn matrix_to_mle<R: Ring>(m: SparseMatrix<R>) -> SparseMultilinearExtension<R> {
     let n_rows = m.nrows().next_power_of_two();
     let n_cols = m.ncols().next_power_of_two();
-    let n_vars: usize = (log2(n_rows) + log2(n_cols)) as usize; // n_vars = s + s'
+    let n_vars: usize = (log2(n_rows * n_cols)) as usize; // n_vars = s + s'
 
     // build the sparse vec representing the sparse matrix
     let mut v: Vec<(usize, R)> = Vec::with_capacity(m.nnz());
 
-    for (col, row, val) in m.triplet_iter() {
+    for (col, row, val) in m.triplet_iter().filter(|(_, _, val)| !val.is_zero()) {
         v.push((row * n_cols + col, *val));
     }
 
@@ -106,105 +108,96 @@ pub fn dense_vec_to_mle<R: Ring>(n_vars: usize, v: &[R]) -> SparseMultilinearExt
     SparseMultilinearExtension::<R>::from_evaluations(n_vars, &v_sparse)
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use crate::{
-//         arith::ccs::tests::get_test_z,
-//         utils::multilinear_polynomial::fix_variables,
-//         utils::multilinear_polynomial::tests::fix_last_variables,
-//         utils::{hypercube::BooleanHypercube, vec::tests::to_F_matrix},
-//     };
-//     use ark_poly::MultilinearExtension;
-//     use ark_std::Zero;
+// Pad a vector by repeating last element
+fn pad_vector(mut vec: Vec<usize>, target_length: usize) -> Vec<usize> {
+    if vec.len() < target_length {
+        if let Some(&last_element) = vec.last() {
+            vec.resize(target_length, last_element);
+        }
+    }
+    vec
+}
 
-//     use lattirust_arithmetic::ring::{Pow2CyclotomicPolyRing, Zq};
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod tests {
+    use super::*;
+    use crate::arith::{r1cs::tests::to_F_matrix, tests::get_test_z};
 
-//     #[test]
-//     fn test_matrix_to_mle() {
-//         let A = to_F_matrix::<Pow2CyclotomicPolyRing<Zq<17912658149728070801u64, 64>>>(vec![
-//             vec![2, 3, 4, 4],
-//             vec![4, 11, 14, 14],
-//             vec![2, 8, 17, 17],
-//             vec![420, 4, 2, 0],
-//         ]);
+    use ark_ff::Zero;
+    use lattirust_arithmetic::{mle::MultilinearExtension, ring::Z2_64};
 
-//         let A_mle = matrix_to_mle(A);
-//         assert_eq!(A_mle.evaluations.len(), 15); // 15 non-zero elements
-//         assert_eq!(A_mle.num_vars, 4); // 4x4 matrix, thus 2bit x 2bit, thus 2^4=16 evals
+    // Function to convert usize to a binary vector of Ring elements.
+    fn usize_to_binary_vector<R: Ring>(n: usize, dimensions: usize) -> Vec<R> {
+        let mut bits = Vec::with_capacity(dimensions);
+        let mut current = n;
 
-//         let A = to_F_matrix::<Fr>(vec![
-//             vec![2, 3, 4, 4, 1],
-//             vec![4, 11, 14, 14, 2],
-//             vec![2, 8, 17, 17, 3],
-//             vec![420, 4, 2, 0, 4],
-//             vec![420, 4, 2, 0, 5],
-//         ]);
-//         let A_mle = matrix_to_mle(A.clone());
-//         assert_eq!(A_mle.evaluations.len(), 23); // 23 non-zero elements
-//         assert_eq!(A_mle.num_vars, 6); // 5x5 matrix, thus 3bit x 3bit, thus 2^6=64 evals
+        for _ in 0..dimensions {
+            if (current & 1) == 1 {
+                bits.push(R::one());
+            } else {
+                bits.push(R::zero());
+            }
+            current >>= 1;
+        }
+        bits
+    }
 
-//         // check that the A_mle evaluated over the boolean hypercube equals the matrix A_i_j values
-//         let bhc = BooleanHypercube::new(A_mle.num_vars);
-//         let A_padded = pad_matrix(&A);
-//         let A_padded_dense = A_padded.to_dense();
-//         for (i, A_row) in A_padded_dense.iter().enumerate() {
-//             for (j, _) in A_row.iter().enumerate() {
-//                 let s_i_j = bhc.at_i(i * A_row.len() + j);
-//                 assert_eq!(A_mle.evaluate(&s_i_j).unwrap(), A_padded_dense[i][j]);
-//             }
-//         }
-//     }
+    // Wrapper function to generate a boolean hypercube.
+    fn boolean_hypercube<R: Ring>(dimensions: usize) -> Vec<Vec<R>> {
+        let max_val = 1 << dimensions; // 2^dimensions
+        (0..max_val)
+            .map(|i| usize_to_binary_vector::<R>(i, dimensions))
+            .collect()
+    }
+    #[test]
+    fn test_matrix_to_mle() {
+        type R = Z2_64;
+        let A = to_F_matrix::<R>(vec![
+            vec![2, 3, 4, 4],
+            vec![4, 11, 14, 14],
+            vec![2, 8, 17, 17],
+            vec![420, 4, 2, 0],
+        ]);
 
-//     #[test]
-//     fn test_vec_to_mle() {
-//         let z = get_test_z::<Fr>(3);
-//         let n_vars = 3;
-//         let z_mle = dense_vec_to_mle(n_vars, &z);
+        let A_mle = matrix_to_mle(A);
+        assert_eq!(A_mle.evaluations.len(), 15); // 15 non-zero elements
+        assert_eq!(A_mle.num_vars, 4); // 4x4 matrix, thus 2bit x 2bit, thus 2^4=16 evals
 
-//         // check that the z_mle evaluated over the boolean hypercube equals the vec z_i values
-//         let bhc = BooleanHypercube::new(z_mle.num_vars);
-//         for (i, z_i) in z.iter().enumerate() {
-//             let s_i = bhc.at_i(i);
-//             assert_eq!(z_mle.evaluate(&s_i).unwrap(), z_i.clone());
-//         }
-//         // for the rest of elements of the boolean hypercube, expect it to evaluate to zero
-//         for i in (z.len())..(1 << z_mle.num_vars) {
-//             let s_i = bhc.at_i(i);
-//             assert_eq!(z_mle.evaluate(&s_i).unwrap(), Fr::zero());
-//         }
-//     }
+        let A = to_F_matrix::<R>(vec![
+            vec![2, 3, 4, 4, 1],
+            vec![4, 11, 14, 14, 2],
+            vec![2, 8, 17, 17, 3],
+            vec![420, 4, 2, 0, 4],
+            vec![420, 4, 2, 0, 5],
+        ]);
+        let A_mle = matrix_to_mle(A.clone());
+        assert_eq!(A_mle.evaluations.len(), 23); // 23 non-zero elements
+        assert_eq!(A_mle.num_vars, 6); // 5x5 matrix, thus 3bit x 3bit, thus 2^6=64 evals
 
-//     #[test]
-//     fn test_fix_variables() {
-//         let A = to_F_matrix(vec![
-//             vec![2, 3, 4, 4],
-//             vec![4, 11, 14, 14],
-//             vec![2, 8, 17, 17],
-//             vec![420, 4, 2, 0],
-//         ]);
+        // check that the A_mle evaluated over the boolean hypercube equals the matrix A_i_j values
+        let _bhc = boolean_hypercube::<R>(2);
 
-//         let A_mle = matrix_to_dense_mle(A.clone());
-//         let A = A.to_dense();
-//         let bhc = BooleanHypercube::new(2);
-//         for (i, y) in bhc.enumerate() {
-//             // First check that the arkworks and espresso funcs match
-//             let expected_fix_left = A_mle.fix_variables(&y); // try arkworks fix_variables
-//             let fix_left = fix_variables(&A_mle, &y); // try espresso fix_variables
-//             assert_eq!(fix_left, expected_fix_left);
+        let _A_dense = matrix_to_dense_mle(A);
+    }
 
-//             // Check that fixing first variables pins down a column
-//             // i.e. fixing x to 0 will return the first column
-//             //      fixing x to 1 will return the second column etc.
-//             let column_i: Vec<Fr> = A.clone().iter().map(|x| x[i]).collect();
-//             assert_eq!(fix_left.evaluations, column_i);
+    #[test]
+    fn test_vec_to_mle() {
+        type R = Z2_64;
+        let z = get_test_z::<R>(3);
+        let n_vars = 3;
+        let z_mle = dense_vec_to_mle(n_vars, &z);
 
-//             // Now check that fixing last variables pins down a row
-//             // i.e. fixing y to 0 will return the first row
-//             //      fixing y to 1 will return the second row etc.
-//             let row_i: Vec<Fr> = A[i].clone();
-//             let fix_right = fix_last_variables(&A_mle, &y);
-//             assert_eq!(fix_right.evaluations, row_i);
-//         }
-//     }
-// }
+        // check that the z_mle evaluated over the boolean hypercube equals the vec z_i values
+        let bhc = boolean_hypercube(z_mle.num_vars);
+
+        for (i, z_i) in z.iter().enumerate() {
+            let s_i = &bhc[i];
+            assert_eq!(z_mle.evaluate(s_i), z_i.clone());
+        }
+        // for the rest of elements of the boolean hypercube, expect it to evaluate to zero
+        for s_i in bhc.iter().take(1 << z_mle.num_vars).skip(z.len()) {
+            assert_eq!(z_mle.fix_variables(s_i)[0], R::zero());
+        }
+    }
+}
