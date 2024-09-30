da19a9838612c561221a10989e80dc1725ebc20a
Ilia Vlasov <elijah.vlasov@gmail.com>
2024-09-30 11:39:20 +0100

    Add challenge sets to the rings

--- cyclotomic-rings/src/rings/goldilocks.rs
+++ cyclotomic-rings/src/rings/goldilocks.rs
@@ -1,8 +1,12 @@
 // PGold = 2^64 − 2^32 + 1
 
-use lattirust_ring::{cyclotomic_ring::models::goldilocks::{Fq, RqNTT, RqPoly}, Ring};
+use lattirust_ring::{
+    cyclotomic_ring::models::goldilocks::{Fq, RqNTT, RqPoly},
+    Ring,
+};
 
 use super::SuitableRing;
+use crate::challenge_set::error;
 use crate::challenge_set::LatticefoldChallengeSet;
 
 pub type GoldilocksRingNTT = RqNTT;
@@ -15,32 +19,90 @@ impl SuitableRing for GoldilocksRingNTT {
 
 pub struct GoldilocksPoseidonConfig;
 
-#[allow(dead_code)]
 pub struct GoldilocksChallengeSet;
 
 const MAX_COEFF: i16 = 32;
 
+/// For Goldilocks prime the challenge set is the set of all
+/// ring elements whose coefficients are in the range [-32, 32[.
 impl LatticefoldChallengeSet<GoldilocksRingNTT> for GoldilocksChallengeSet {
+    /// To generate an element in [-32, 32[ it is enough to use 6 bits.
+    /// Thus to generate 24 coefficients in that range 18 bytes is enough.
     const BYTES_NEEDED: usize = 18;
 
     fn small_challenge_from_random_bytes(
         bs: &[u8],
-    ) -> Result<GoldilocksRingPoly, crate::challenge_set::error::ChallengeSetError> {
+    ) -> Result<GoldilocksRingPoly, error::ChallengeSetError> {
         if bs.len() != Self::BYTES_NEEDED {
-            return Err(crate::challenge_set::error::ChallengeSetError::TooFewBytes(bs.len(), Self::BYTES_NEEDED));
+            return Err(error::ChallengeSetError::TooFewBytes(
+                bs.len(),
+                Self::BYTES_NEEDED,
+            ));
         }
 
         let mut coeffs: Vec<Fq> = Vec::with_capacity(24);
 
         for i in 0..6 {
-            let x0: i16 = ((bs[3 * i] & 0b0011_1111)) as i16 - MAX_COEFF;
-            let x1: i16 = (((bs[3 * i] & 0b1100_0000) >> 6) | ((bs[3 * i + 1] & 0b0000_1111) << 2)) as i16 - MAX_COEFF;
-            let x2: i16 = (((bs[3 * i + 1] & 0b1111_0000) >> 4) | ((bs[3 * i + 2] & 0b0000_0011) << 4)) as i16 - MAX_COEFF;
-            let x3: i16 = (((bs[3 * i + 2] & 0b1111_1100) >> 2)) as i16 - MAX_COEFF;
+            let x0: i16 = (bs[3 * i] & 0b0011_1111) as i16 - MAX_COEFF;
+            let x1: i16 = (((bs[3 * i] & 0b1100_0000) >> 6) | ((bs[3 * i + 1] & 0b0000_1111) << 2))
+                as i16
+                - MAX_COEFF;
+            let x2: i16 = (((bs[3 * i + 1] & 0b1111_0000) >> 4)
+                | ((bs[3 * i + 2] & 0b0000_0011) << 4)) as i16
+                - MAX_COEFF;
+            let x3: i16 = ((bs[3 * i + 2] & 0b1111_1100) >> 2) as i16 - MAX_COEFF;
 
             coeffs.extend_from_slice(&[Fq::from(x0), Fq::from(x1), Fq::from(x2), Fq::from(x3)]);
         }
-        
+
         Ok(GoldilocksRingPoly::from(coeffs))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ark_ff::BigInt;
+    use lattirust_ring::cyclotomic_ring::models::goldilocks::Fq;
+
+    use super::*;
+
+    #[test]
+    fn test_small_challenge_from_random_bytes() {
+        let challenge = GoldilocksChallengeSet::small_challenge_from_random_bytes(&[
+            0x7b, 0x4b, 0xe5, 0x8e, 0xe5, 0x11, 0xd2, 0xd0, 0x9c, 0x22, 0xba, 0x2e, 0xeb, 0xa8,
+            0xba, 0x35, 0xf2, 0x18,
+        ])
+        .unwrap();
+
+        let res_coeffs: Vec<Fq> = vec![
+            Fq::new(BigInt([27])),
+            Fq::new(BigInt([13])),
+            Fq::new(BigInt([18446744069414584309])),
+            Fq::new(BigInt([25])),
+            Fq::new(BigInt([18446744069414584303])),
+            Fq::new(BigInt([18446744069414584311])),
+            Fq::new(BigInt([18446744069414584319])),
+            Fq::new(BigInt([18446744069414584293])),
+            Fq::new(BigInt([18446744069414584307])),
+            Fq::new(BigInt([18446744069414584292])),
+            Fq::new(BigInt([18446744069414584302])),
+            Fq::new(BigInt([7])),
+            Fq::new(BigInt([2])),
+            Fq::new(BigInt([8])),
+            Fq::new(BigInt([11])),
+            Fq::new(BigInt([18446744069414584300])),
+            Fq::new(BigInt([11])),
+            Fq::new(BigInt([3])),
+            Fq::new(BigInt([10])),
+            Fq::new(BigInt([14])),
+            Fq::new(BigInt([21])),
+            Fq::new(BigInt([18446744069414584297])),
+            Fq::new(BigInt([18446744069414584304])),
+            Fq::new(BigInt([18446744069414584295])),
+        ];
+
+        let expected = GoldilocksRingPoly::from(res_coeffs);
+
+        assert_eq!(expected, challenge)
     }
 }
--- cyclotomic-rings/src/rings/stark.rs
+++ cyclotomic-rings/src/rings/stark.rs
@@ -1,5 +1,7 @@
 // PGold = 2^64 − 2^32 + 1
-use lattirust_ring::cyclotomic_ring::models::stark_prime::{RqNTT, RqPoly};
+use lattirust_ring::cyclotomic_ring::models::stark_prime::{Fq, RqNTT, RqPoly};
+
+use crate::{challenge_set::error, challenge_set::LatticefoldChallengeSet};
 
 use super::SuitableRing;
 
@@ -14,18 +16,65 @@ impl SuitableRing for StarkRingNTT {
 
 pub struct StarkPoseidonConfig;
 
-#[allow(dead_code)]
 pub struct StarkChallengeSet;
 
-// impl LatticefoldChallengeSet<GoldilocksRingNTT> for PGoldChallengeSet {
-//     fn small_challenge_coefficient_from_random_bytes(
-//         _i: usize,
-//         bs: &[u8],
-//     ) -> <GoldilocksRingPoly as PolyRing>::BaseRing {
-//         if bs[0] == 0 {
-//             Fq3::ZERO
-//         } else {
-//             Fq3::ONE
-//         }
-//     }
-// }
+/// Small challenges are the ring elements with coefficients in range [0; 2^8[.
+impl LatticefoldChallengeSet<StarkRingNTT> for StarkChallengeSet {
+    const BYTES_NEEDED: usize = 16;
+
+    fn small_challenge_from_random_bytes(
+        bs: &[u8],
+    ) -> Result<<StarkRingNTT as SuitableRing>::CoefficientRepresentation, error::ChallengeSetError>
+    {
+        if bs.len() != Self::BYTES_NEEDED {
+            return Err(error::ChallengeSetError::TooFewBytes(
+                bs.len(),
+                Self::BYTES_NEEDED,
+            ));
+        }
+
+        Ok(StarkRingPoly::from(
+            bs.iter().map(|&x| Fq::from(x)).collect::<Vec<Fq>>(),
+        ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ark_ff::BigInt;
+    use lattirust_ring::PolyRing;
+
+    use super::*;
+
+    #[test]
+    fn test_small_challenge_from_random_bytes() {
+        let challenge = StarkChallengeSet::small_challenge_from_random_bytes(&[
+            0x7b, 0x4b, 0xe5, 0x8e, 0xe5, 0x11, 0xd2, 0xd0, 0x9c, 0x22, 0xba, 0x2e, 0xeb, 0xa8,
+            0xba, 0x35,
+        ])
+        .unwrap();
+
+        let res_coeffs: Vec<Fq> = vec![
+            Fq::new(BigInt([123, 0, 0, 0])),
+            Fq::new(BigInt([75, 0, 0, 0])),
+            Fq::new(BigInt([229, 0, 0, 0])),
+            Fq::new(BigInt([142, 0, 0, 0])),
+            Fq::new(BigInt([229, 0, 0, 0])),
+            Fq::new(BigInt([17, 0, 0, 0])),
+            Fq::new(BigInt([210, 0, 0, 0])),
+            Fq::new(BigInt([208, 0, 0, 0])),
+            Fq::new(BigInt([156, 0, 0, 0])),
+            Fq::new(BigInt([34, 0, 0, 0])),
+            Fq::new(BigInt([186, 0, 0, 0])),
+            Fq::new(BigInt([46, 0, 0, 0])),
+            Fq::new(BigInt([235, 0, 0, 0])),
+            Fq::new(BigInt([168, 0, 0, 0])),
+            Fq::new(BigInt([186, 0, 0, 0])),
+            Fq::new(BigInt([53, 0, 0, 0])),
+        ];
+
+        let expected = StarkRingPoly::from(res_coeffs);
+
+        assert_eq!(expected, challenge)
+    }
+}
