1e5e1bef84b9bb99a1e9f0c4be0510723c950036
Isaac <isaacvil@gmail.com>
2024-10-07 16:17:53 -0600

    Macros for benches

--- latticefold/benches/ajtai.rs
+++ latticefold/benches/ajtai.rs
@@ -70,15 +70,16 @@ fn ajtai_benchmark<
         },
     );
 
-    group.bench_with_input(
-        BenchmarkId::new("DecomposeCommitNTT", format!("C={},W={},B={}", C, W, B)),
-        &(ajtai_data_2, witness_2),
-        |b, (ajtai_data, witness)| {
-            b.iter(|| {
-                let _ = ajtai_data.decompose_and_commit_ntt::<P>(witness);
-            })
-        },
-    );
+    let _ = ajtai_data_2.decompose_and_commit_ntt::<P>(&witness_2);
+    // group.bench_with_input(
+    //     BenchmarkId::new("DecomposeCommitNTT", format!("C={},W={},B={}", C, W, B)),
+    //     &(ajtai_data_2, witness_2),
+    //     |b, (ajtai_data, witness)| {
+    //         b.iter(|| {
+    //             let _ = ajtai_data.decompose_and_commit_ntt::<P>(witness);
+    //         })
+    //     },
+    // );
 }
 
 macro_rules! run_stark_prime_ajtai_benchmarks {
@@ -97,11 +98,46 @@ macro_rules! run_goldilocks_ajtai_benchmarks {
         )+
     };
 }
+// macro_rules! run_babybear_ajtai_benchmarks {
+//     ($crit:expr, $cw: expr, $(($w:expr, $b:expr)),+ ) => {
+//         $(
+//             // BabyBear
+//             struct BabyBearParamsWith$b;
+//             impl DecompositionParams for BabyBearParamsWith$b {
+//                 const B: u128 = 1 << 28;
+//                 const L: usize = 2;
+//                 const B_SMALL: usize = 2;
+//                 const K: usize = 28;
+//             }
+//             ajtai_benchmark::<$cw, $w, $b, BabyBearRingNTT, BabyBearParams>($crit);
+//         )+
+//     };
+// }
+
+use paste;
+macro_rules! define_params {
+    ($b:expr, $l:expr) => {
+        paste::paste! {
+            #[derive(Clone)]
+            struct [<BabyBearParamsWithB $b>];
+
+            impl DecompositionParams for [<BabyBearParamsWithB $b>] {
+                const B: u128 = $b;
+                const L: usize = $l;
+                const B_SMALL: usize = 2;
+                const K: usize = 28;
+            }
+        }
+    };
+}
+
 macro_rules! run_babybear_ajtai_benchmarks {
-    ($crit:expr, $cw: expr, $(($w:expr, $b:expr)),+ ) => {
+    ($crit:expr, $cw:expr, $(($w:expr, $b:expr, $l:expr)),+ ) => {
         $(
-            // BabyBear
-            ajtai_benchmark::<$cw, $w, $b, BabyBearRingNTT, BabyBearParams>($crit);
+            define_params!($b, $l);
+            paste::paste! {
+                ajtai_benchmark::<$cw, $w, $b, BabyBearRingNTT, [<BabyBearParamsWithB $b>]>($crit);
+            }
         )+
     };
 }
@@ -111,76 +147,66 @@ fn ajtai_benchmarks(c: &mut Criterion) {
     let mut group = c.benchmark_group("Ajtai BabyBear");
     group.plot_config(plot_config.clone());
     {
-        run_babybear_ajtai_benchmarks!(
-            &mut group,
-            1,
-            ({ 1 << 15 }, 2), // B = 1.646
-            ({ 1 << 16 }, 2)  // B = 1.164
-                              // ({ 1 << 17 }, 2), // B = 0.823
-                              // ({ 1 << 18 }, 2), // B < 1 bellow
-                              // ({ 1 << 19 }, 2),
-                              // ({ 1 << 20 }, 2)
-        );
-        run_babybear_ajtai_benchmarks!(
-            &mut group,
-            2,
-            ({ 1 << 15 }, 2), // B = 1.646
-            ({ 1 << 16 }, 2)  // B = 1.164
-                              // ({ 1 << 17 }, 2), // B = 0.823
-                              // ({ 1 << 18 }, 2), // B < 1 bellow
-                              // ({ 1 << 19 }, 2),
-                              // ({ 1 << 20 }, 2)
-        );
+        // run_babybear_ajtai_benchmarks!(
+        //     &mut group,
+        //     1,
+        //     ({ 1 << 15 }, 2), // B = 1.646
+        //     ({ 1 << 16 }, 2)  // B = 1.164
+        //                       // ({ 1 << 17 }, 2), // B = 0.823
+        //                       // ({ 1 << 18 }, 2), // B < 1 bellow
+        //                       // ({ 1 << 19 }, 2),
+        //                       // ({ 1 << 20 }, 2)
+        // );
         run_babybear_ajtai_benchmarks!(
             &mut group,
             2,
-            ({ 1 << 15 }, 43), // B = 42.260
-            ({ 1 << 16 }, 30), // B = 29.882
-            ({ 1 << 17 }, 22), // B = 21.130
-            ({ 1 << 18 }, 15), // B = 14.941
-            ({ 1 << 19 }, 11), // B = 10.565
-            ({ 1 << 20 }, 8)   // B = 7.471
+            // ({ 1 << 15 }, 43, 6) // B = 42.260
+            ({ 1 << 16 }, 30, 7) // B = 29.882
+                                 // ({ 1 << 17 }, 22, 7), // B = 21.130
+                                 // ({ 1 << 18 }, 15, 8), // B = 14.941
+                                 // ({ 1 << 19 }, 11, 9), // B = 10.565
+                                 // ({ 1 << 20 }, 8, 11)  // B = 7.471
         );
-        run_babybear_ajtai_benchmarks!(
-            &mut group,
-            3,
-            ({ 1 << 15 }, 510), // B = 509.889
-            ({ 1 << 16 }, 361), // B = 360.546
-            ({ 1 << 17 }, 255), // B = 254.945
-            ({ 1 << 18 }, 181), // B = 180.273
-            ({ 1 << 19 }, 128), // B = 127.472
-            ({ 1 << 20 }, 91)   // B = 90.137
-        );
-        run_babybear_ajtai_benchmarks!(
-            &mut group,
-            4,
-            ({ 1 << 15 }, 4162), // B = 4161.599
-            ({ 1 << 16 }, 2943), // B = 2942.694
-            ({ 1 << 17 }, 2081), // B = 2080.799
-            ({ 1 << 18 }, 1472), // B = 1471.347
-            ({ 1 << 19 }, 1041), // B = 1040.3996
-            ({ 1 << 20 }, 736)   // B = 735.674
-        );
-        run_babybear_ajtai_benchmarks!(
-            &mut group,
-            5,
-            ({ 1 << 15 }, 26459), // B = 26458.082
-            ({ 1 << 16 }, 18709), // B = 18708.689
-            ({ 1 << 17 }, 13230), // B = 13229.041
-            ({ 1 << 18 }, 9355),  // B = 9354.345
-            ({ 1 << 19 }, 6615),  // B = 6614.521
-            ({ 1 << 20 }, 4678)   // B = 4677.172
-        );
-        run_babybear_ajtai_benchmarks!(
-            &mut group,
-            6,
-            ({ 1 << 15 }, 140863), // B = 140862.511
-            ({ 1 << 16 }, 99605),  // B = 99604.837
-            ({ 1 << 17 }, 70432),  // B = 70431.256
-            ({ 1 << 18 }, 49803),  // B = 49802.418
-            ({ 1 << 19 }, 35216),  // B = 35215.628
-            ({ 1 << 20 }, 24902)   // B = 24901.209
-        );
+        // run_babybear_ajtai_benchmarks!(
+        //     &mut group,
+        //     3,
+        //     ({ 1 << 15 }, 510), // B = 509.889
+        //     ({ 1 << 16 }, 361), // B = 360.546
+        //     ({ 1 << 17 }, 255), // B = 254.945
+        //     ({ 1 << 18 }, 181), // B = 180.273
+        //     ({ 1 << 19 }, 128), // B = 127.472
+        //     ({ 1 << 20 }, 91)   // B = 90.137
+        // );
+        // run_babybear_ajtai_benchmarks!(
+        //     &mut group,
+        //     4,
+        //     ({ 1 << 15 }, 4162), // B = 4161.599
+        //     ({ 1 << 16 }, 2943), // B = 2942.694
+        //     ({ 1 << 17 }, 2081), // B = 2080.799
+        //     ({ 1 << 18 }, 1472), // B = 1471.347
+        //     ({ 1 << 19 }, 1041), // B = 1040.3996
+        //     ({ 1 << 20 }, 736)   // B = 735.674
+        // );
+        // run_babybear_ajtai_benchmarks!(
+        //     &mut group,
+        //     5,
+        //     ({ 1 << 15 }, 26459), // B = 26458.082
+        //     ({ 1 << 16 }, 18709), // B = 18708.689
+        //     ({ 1 << 17 }, 13230), // B = 13229.041
+        //     ({ 1 << 18 }, 9355),  // B = 9354.345
+        //     ({ 1 << 19 }, 6615),  // B = 6614.521
+        //     ({ 1 << 20 }, 4678)   // B = 4677.172
+        // );
+        // run_babybear_ajtai_benchmarks!(
+        //     &mut group,
+        //     6,
+        //     ({ 1 << 15 }, 140863), // B = 140862.511
+        //     ({ 1 << 16 }, 99605),  // B = 99604.837
+        //     ({ 1 << 17 }, 70432),  // B = 70431.256
+        //     ({ 1 << 18 }, 49803),  // B = 49802.418
+        //     ({ 1 << 19 }, 35216),  // B = 35215.628
+        //     ({ 1 << 20 }, 24902)   // B = 24901.209
+        // );
         group.finish();
     }
 
