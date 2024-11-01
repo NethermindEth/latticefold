f051ae569df7fdc73f87fe9c8de437abcf3c1132
Ilia Vlasov <elijah.vlasov@gmail.com>
2024-11-01 17:25:56 +0000

    Add goldilocks test

--- latticefold/src/nifs/folding.rs
+++ latticefold/src/nifs/folding.rs
@@ -495,6 +495,175 @@ mod tests {
 }
 
 #[cfg(test)]
+mod tests_goldilocks {
+    use rand::thread_rng;
+
+    use crate::{
+        arith::{r1cs::get_test_z_split, tests::get_test_ccs, Witness, CCCS},
+        commitment::AjtaiCommitmentScheme,
+        decomposition_parameters::DecompositionParams,
+        nifs::{
+            decomposition::{
+                DecompositionProver, DecompositionVerifier, LFDecompositionProver,
+                LFDecompositionVerifier,
+            },
+            folding::{FoldingProver, FoldingVerifier, LFFoldingProver, LFFoldingVerifier},
+            linearization::{
+                LFLinearizationProver, LFLinearizationVerifier, LinearizationProver,
+                LinearizationVerifier,
+            },
+        },
+        transcript::poseidon::PoseidonTranscript,
+    };
+    use cyclotomic_rings::{GoldilocksChallengeSet, GoldilocksRingNTT};
+
+    // Boilerplate code to generate values needed for testing
+    type R = GoldilocksRingNTT;
+    type CS = GoldilocksChallengeSet;
+    type T = PoseidonTranscript<GoldilocksRingNTT, CS>;
+
+    #[derive(Clone)]
+    struct PP;
+
+    impl DecompositionParams for PP {
+        const B: u128 = 1_024;
+        const L: usize = 1;
+        const B_SMALL: usize = 2;
+        const K: usize = 10;
+    }
+
+    #[test]
+    fn test_folding() {
+        const WIT_LEN: usize = 4; // 4 is the length of witness in this (Vitalik's) example
+        const W: usize = WIT_LEN * PP::L; // the number of columns of the Ajtai matrix
+
+        let ccs = get_test_ccs::<R>(W);
+        let (_, x_ccs, w_ccs) = get_test_z_split::<R>(3);
+        let scheme = AjtaiCommitmentScheme::rand(&mut thread_rng());
+        let wit: Witness<R> = Witness::from_w_ccs::<PP>(&w_ccs);
+        let cm_i: CCCS<4, R> = CCCS {
+            cm: wit.commit::<4, 4, PP>(&scheme).unwrap(),
+            x_ccs,
+        };
+
+        let mut prover_transcript = PoseidonTranscript::<R, CS>::default();
+        let mut verifier_transcript = PoseidonTranscript::<R, CS>::default();
+
+        let (_, linearization_proof) =
+            LFLinearizationProver::<_, T>::prove(&cm_i, &wit, &mut prover_transcript, &ccs)
+                .unwrap();
+
+        let lcccs = LFLinearizationVerifier::<_, PoseidonTranscript<R, CS>>::verify(
+            &cm_i,
+            &linearization_proof,
+            &mut verifier_transcript,
+            &ccs,
+        )
+        .unwrap();
+
+        let (_, vec_wit, decomposition_proof) = LFDecompositionProver::<_, T>::prove::<4, 4, PP>(
+            &lcccs,
+            &wit,
+            &mut prover_transcript,
+            &ccs,
+            &scheme,
+        )
+        .unwrap();
+
+        let vec_lcccs = LFDecompositionVerifier::<_, T>::verify::<4, PP>(
+            &lcccs,
+            &decomposition_proof,
+            &mut verifier_transcript,
+            &ccs,
+        )
+        .unwrap();
+        let (lcccs, wit_s) = {
+            let mut lcccs = vec_lcccs.clone();
+            let mut lcccs_r = vec_lcccs;
+            lcccs.append(&mut lcccs_r);
+
+            let mut wit_s = vec_wit.clone();
+            let mut wit_s_r = vec_wit;
+            wit_s.append(&mut wit_s_r);
+
+            (lcccs, wit_s)
+        };
+        let (lcccs_prover, _, folding_proof) =
+            LFFoldingProver::<_, T>::prove::<4, PP>(&lcccs, &wit_s, &mut prover_transcript, &ccs)
+                .unwrap();
+
+        let lcccs_verifier = LFFoldingVerifier::<_, T>::verify::<4, PP>(
+            &lcccs,
+            &folding_proof,
+            &mut verifier_transcript,
+            &ccs,
+        )
+        .unwrap();
+
+        assert_eq!(lcccs_prover, lcccs_verifier);
+    }
+
+    #[test]
+    fn test_failing_folding_prover() {
+        const WIT_LEN: usize = 4; // 4 is the length of witness in this (Vitalik's) example
+        const W: usize = WIT_LEN * PP::L; // the number of columns of the Ajtai matrix
+
+        let ccs = get_test_ccs::<R>(W);
+        let (_, x_ccs, w_ccs) = get_test_z_split::<R>(3);
+        let scheme = AjtaiCommitmentScheme::rand(&mut thread_rng());
+        let wit: Witness<R> = Witness::from_w_ccs::<PP>(&w_ccs);
+        let cm_i: CCCS<4, R> = CCCS {
+            cm: wit.commit::<4, 4, PP>(&scheme).unwrap(),
+            x_ccs,
+        };
+
+        let mut prover_transcript = PoseidonTranscript::<R, CS>::default();
+        let mut verifier_transcript = PoseidonTranscript::<R, CS>::default();
+
+        let (_, linearization_proof) =
+            LFLinearizationProver::<_, T>::prove(&cm_i, &wit, &mut prover_transcript, &ccs)
+                .unwrap();
+
+        let lcccs = LFLinearizationVerifier::<_, PoseidonTranscript<R, CS>>::verify(
+            &cm_i,
+            &linearization_proof,
+            &mut verifier_transcript,
+            &ccs,
+        )
+        .unwrap();
+
+        let (_, mut vec_wit, decomposition_proof) =
+            LFDecompositionProver::<_, T>::prove::<4, 4, PP>(
+                &lcccs,
+                &wit,
+                &mut prover_transcript,
+                &ccs,
+                &scheme,
+            )
+            .unwrap();
+
+        let vec_lcccs = LFDecompositionVerifier::<_, T>::verify::<4, PP>(
+            &lcccs,
+            &decomposition_proof,
+            &mut verifier_transcript,
+            &ccs,
+        )
+        .unwrap();
+
+        vec_wit[0] = Witness::<R>::from_w_ccs::<PP>(&w_ccs);
+
+        let res = LFFoldingProver::<_, T>::prove::<4, PP>(
+            &vec_lcccs,
+            &vec_wit,
+            &mut prover_transcript,
+            &ccs,
+        );
+
+        assert!(res.is_err())
+    }
+}
+
+#[cfg(test)]
 mod tests_stark {
     use lattirust_ring::cyclotomic_ring::models::stark_prime::RqNTT;
     use num_bigint::BigUint;
