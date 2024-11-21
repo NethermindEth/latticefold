2a1ad12da7261b11fc24fb2ba32b723d436d93be
v0-e <xyz.vieira@gmail.com>
2024-11-21 15:25:57 +0000

    Import order in R1CS

--- latticefold/src/arith/r1cs.rs
+++ latticefold/src/arith/r1cs.rs
@@ -1,5 +1,6 @@
 use lattirust_linear_algebra::sparse_matrix::dense_matrix_to_sparse;
 use lattirust_linear_algebra::SparseMatrix;
+use lattirust_ring::Ring;
 
 use super::{
     error::CSError as Error,
@@ -7,7 +8,6 @@ use super::{
 };
 use crate::arith::hadamard;
 use crate::ark_base::*;
-use lattirust_ring::Ring;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct R1CS<R: Ring> {
