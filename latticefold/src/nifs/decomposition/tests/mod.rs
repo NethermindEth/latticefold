ae146c7b874d9ea35b28ca7e14bc56ab9ea1f74a
Marko Cupic <marko.cupic@nethermind.io>
2024-11-26 19:02:43 +0100

    Implement test_recompose_commitment

--- latticefold/src/nifs/decomposition/tests/mod.rs
+++ latticefold/src/nifs/decomposition/tests/mod.rs
@@ -1,20 +1,22 @@
-use cyclotomic_rings::{challenge_set::LatticefoldChallengeSet, rings::SuitableRing};
-use lattirust_poly::mle::DenseMultilinearExtension;
-use rand::Rng;
-
+use crate::arith::r1cs::get_test_z_split;
+use crate::arith::tests::get_test_ccs;
+use crate::arith::utils::mat_vec_mul;
+use crate::arith::{Witness, CCS, LCCCS};
+use crate::commitment::{AjtaiCommitmentScheme, Commitment};
+use crate::decomposition_parameters::test_params::GoldilocksDP;
+use crate::decomposition_parameters::DecompositionParams;
+use crate::nifs::decomposition::{
+    DecompositionProver, LFDecompositionProver, LFDecompositionVerifier,
+};
 use crate::nifs::error::DecompositionError;
 use crate::nifs::linearization::utils::compute_u;
 use crate::nifs::mle_helpers::{evaluate_mles, to_mles};
-use crate::{
-    arith::{r1cs::get_test_z_split, tests::get_test_ccs, utils::mat_vec_mul, Witness, CCS, LCCCS},
-    ark_base::*,
-    commitment::{AjtaiCommitmentScheme, Commitment},
-    decomposition_parameters::DecompositionParams,
-    nifs::decomposition::{
-        DecompositionProver, DecompositionVerifier, LFDecompositionProver, LFDecompositionVerifier,
-    },
-    transcript::poseidon::PoseidonTranscript,
-};
+use crate::transcript::poseidon::PoseidonTranscript;
+use ark_std::vec::Vec;
+use cyclotomic_rings::challenge_set::LatticefoldChallengeSet;
+use cyclotomic_rings::rings::{GoldilocksChallengeSet, GoldilocksRingNTT, SuitableRing};
+use lattirust_poly::mle::DenseMultilinearExtension;
+use rand::Rng;
 
 fn generate_decomposition_args<RqNTT, CS, DP, const WIT_LEN: usize, const W: usize>() -> (
     LCCCS<4, RqNTT>,
@@ -38,7 +40,6 @@ where
     let (_, x_ccs, _) = get_test_z_split::<RqNTT>(input);
 
     let wit = Witness::rand::<_, DP>(&mut rng, WIT_LEN);
-
     let mut z: Vec<RqNTT> = Vec::with_capacity(x_ccs.len() + WIT_LEN + 1);
 
     z.extend_from_slice(&x_ccs);
@@ -81,127 +82,35 @@ where
     )
 }
 
-fn test_decomposition<RqNTT, CS, DP, const WIT_LEN: usize, const W: usize>()
-where
-    RqNTT: SuitableRing,
-    CS: LatticefoldChallengeSet<RqNTT>,
-    DP: DecompositionParams,
-{
-    let (lcccs, mut verifier_transcript, mut prover_transcript, ccs, wit, scheme) =
+#[test]
+fn test_recompose_commitment() {
+    type CS = GoldilocksChallengeSet;
+    type RqNTT = GoldilocksRingNTT;
+    type DP = GoldilocksDP;
+    type T = PoseidonTranscript<RqNTT, CS>;
+    const WIT_LEN: usize = 4;
+    const W: usize = WIT_LEN * DP::L;
+    const C: usize = 4;
+
+    let (lcccs, _, mut prover_transcript, ccs, wit, scheme) =
         generate_decomposition_args::<RqNTT, CS, DP, WIT_LEN, W>();
 
-    let (_, _, decomposition_proof) =
-        LFDecompositionProver::<_, PoseidonTranscript<RqNTT, CS>>::prove::<W, 4, DP>(
-            &lcccs,
-            &wit,
-            &mut prover_transcript,
-            &ccs,
-            &scheme,
-        )
-        .unwrap();
-
-    let res = LFDecompositionVerifier::<_, PoseidonTranscript<RqNTT, CS>>::verify::<4, DP>(
+    let (_, _, proof) = LFDecompositionProver::<_, T>::prove::<W, C, DP>(
         &lcccs,
-        &decomposition_proof,
-        &mut verifier_transcript,
+        &wit,
+        &mut prover_transcript,
         &ccs,
-    );
-    assert!(res.is_ok())
-}
+        &scheme,
+    )
+    .unwrap();
 
-mod stark {
-    use crate::arith::r1cs::get_test_dummy_z_split;
-    use crate::arith::tests::get_test_dummy_ccs;
-    use crate::arith::{Witness, CCCS};
-    use crate::commitment::AjtaiCommitmentScheme;
-    use crate::decomposition_parameters::{test_params::StarkDP as DP, DecompositionParams};
-    use crate::nifs::linearization::{
-        LFLinearizationProver, LFLinearizationVerifier, LinearizationProver, LinearizationVerifier,
-    };
-    use crate::transcript::poseidon::PoseidonTranscript;
-    use cyclotomic_rings::rings::StarkChallengeSet;
-    use lattirust_ring::cyclotomic_ring::models::stark_prime::RqNTT;
+    let b_s: Vec<_> = (0..DP::K)
+        .map(|i| RqNTT::from((DP::B_SMALL as u128).pow(i as u32)))
+        .collect();
 
-    type CS = StarkChallengeSet;
-    const WIT_LEN: usize = 4;
-    const W: usize = WIT_LEN * DP::L;
-    #[test]
-    fn test_decomposition() {
-        super::test_decomposition::<RqNTT, CS, DP, WIT_LEN, W>();
-    }
+    let should_equal_y0 =
+        LFDecompositionVerifier::<RqNTT, T>::recompose_commitment::<C>(&proof.y_s, &b_s)
+            .expect("Recomposing proof failed");
 
-    #[test]
-    fn test_dummy_decomposition() {
-        type R = RqNTT;
-        type CS = StarkChallengeSet;
-        type T = PoseidonTranscript<R, CS>;
-
-        const C: usize = 16;
-        const X_LEN: usize = 1;
-        const WIT_LEN: usize = 2048;
-        const W: usize = WIT_LEN * DP::L; // the number of columns of the Ajtai matrix
-        let r1cs_rows_size = X_LEN + WIT_LEN + 1; // Let's have a square matrix
-        let ccs = get_test_dummy_ccs::<R, X_LEN, WIT_LEN, W>(r1cs_rows_size, DP::L);
-        let (_, x_ccs, w_ccs) = get_test_dummy_z_split::<R, X_LEN, WIT_LEN>();
-        let mut rng = ark_std::test_rng();
-        let scheme = AjtaiCommitmentScheme::rand(&mut rng);
-        let wit = Witness::from_w_ccs::<DP>(w_ccs);
-
-        let cm_i = CCCS {
-            cm: wit.commit::<C, W, DP>(&scheme).unwrap(),
-            x_ccs,
-        };
-        let mut transcript = PoseidonTranscript::<R, CS>::default();
-        let res = LFLinearizationProver::<_, T>::prove(&cm_i, &wit, &mut transcript, &ccs);
-        let mut transcript = PoseidonTranscript::<R, CS>::default();
-        let res = LFLinearizationVerifier::<_, PoseidonTranscript<R, CS>>::verify(
-            &cm_i,
-            &res.expect("Linearization proof generation error").1,
-            &mut transcript,
-            &ccs,
-        );
-        res.expect("Linearization Verification error");
-    }
-}
-
-mod goldilocks {
-    use crate::decomposition_parameters::{test_params::GoldilocksDP as DP, DecompositionParams};
-    use cyclotomic_rings::rings::GoldilocksChallengeSet;
-    use lattirust_ring::cyclotomic_ring::models::goldilocks::RqNTT;
-    type CS = GoldilocksChallengeSet;
-
-    const WIT_LEN: usize = 4;
-    const W: usize = WIT_LEN * DP::L;
-    #[test]
-    fn test_decomposition() {
-        super::test_decomposition::<RqNTT, CS, DP, WIT_LEN, W>();
-    }
-}
-
-mod frog {
-    use crate::decomposition_parameters::{test_params::GoldilocksDP as DP, DecompositionParams};
-    use cyclotomic_rings::rings::FrogChallengeSet;
-    use lattirust_ring::cyclotomic_ring::models::frog_ring::RqNTT;
-    type CS = FrogChallengeSet;
-
-    const WIT_LEN: usize = 4;
-    const W: usize = WIT_LEN * DP::L;
-    #[test]
-    fn test_decomposition() {
-        super::test_decomposition::<RqNTT, CS, DP, WIT_LEN, W>();
-    }
-}
-
-mod babybear {
-    use crate::decomposition_parameters::{test_params::BabyBearDP as DP, DecompositionParams};
-    use cyclotomic_rings::rings::BabyBearChallengeSet;
-    use lattirust_ring::cyclotomic_ring::models::babybear::RqNTT;
-    type CS = BabyBearChallengeSet;
-
-    const WIT_LEN: usize = 4;
-    const W: usize = WIT_LEN * DP::L;
-    #[test]
-    fn test_decomposition() {
-        super::test_decomposition::<RqNTT, CS, DP, WIT_LEN, W>();
-    }
+    assert_eq!(should_equal_y0, lcccs.cm);
 }
