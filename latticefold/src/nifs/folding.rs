b1368dcfc5e701f2fbb76ac4fe52b603f3945e57
Alex Klein <matthewklein345@gmail.com>
2024-12-01 21:27:41 +0000

    Small change

--- latticefold/src/nifs/folding.rs
+++ latticefold/src/nifs/folding.rs
@@ -176,7 +176,7 @@ impl<NTT: SuitableRing, T: TranscriptWithShortChallenges<NTT>> FoldingProver<NTT
             let mut result = vals[0] * vals[1]
                 + vals[P::K * P::B_SMALL as usize + 3] * vals[P::K * P::B_SMALL as usize + 4];
             result += &vals[2..P::K * P::B_SMALL as usize + 2].iter().product();
-            result += &vals[P::K * P::B_SMALL as usize + 4..2 * P::K * P::B_SMALL as usize + 4]
+            result += &vals[P::K * P::B_SMALL as usize + 5..2 * P::K * P::B_SMALL as usize + 5]
                 .iter()
                 .product();
             result
