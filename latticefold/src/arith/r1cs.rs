08d6a583129b9b2a6f5340346ce91a278e28a7b3
Alex Klein <matthewklein345@gmail.com>
2024-12-04 10:13:29 +0000

    Small improvement to ntt z generation

--- latticefold/src/arith/r1cs.rs
+++ latticefold/src/arith/r1cs.rs
@@ -187,8 +187,8 @@ pub fn get_test_z_ntt<R: SuitableRing>() -> Vec<R> {
     let mut ret: Vec<R> = Vec::new();
     for j in 0..res[0].len() {
         let mut vec = Vec::new();
-        for i in 0..res.len() {
-            vec.push(res[i][j]);
+        for witness in &res {
+            vec.push(witness[j]);
         }
         ret.push(R::from(vec));
     }
@@ -213,29 +213,8 @@ pub fn get_test_z_split<R: Ring>(input: usize) -> (R, Vec<R>, Vec<R>) {
 }
 
 pub fn get_test_z_ntt_split<R: SuitableRing>() -> (R, Vec<R>, Vec<R>) {
-    let mut res = Vec::new();
-    for input in 0..R::dimension() {
-        // z = (1, io, w)
-        res.push(to_F_vec::<R::BaseRing>(vec![
-            input, // io
-            1,
-            input * input * input + input + 5, // x^3 + x + 5
-            input * input,                     // x^2
-            input * input * input,             // x^2 * x
-            input * input * input + input,     // x^3 + x
-        ]))
-    }
-
-    let mut ret: Vec<R> = Vec::new();
-    for j in 0..res[0].len() {
-        let mut vec = Vec::new();
-        for i in 0..res.len() {
-            vec.push(res[i][j]);
-        }
-        ret.push(R::from(vec));
-    }
-
-    (ret[1], vec![ret[0]], ret[2..].to_vec())
+    let z = get_test_z_ntt();
+    (z[1], vec![z[0]], z[2..].to_vec())
 }
 
 pub fn get_test_dummy_z_split<R: Ring, const X_LEN: usize, const WIT_LEN: usize>(
