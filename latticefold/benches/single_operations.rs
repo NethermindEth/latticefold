426053cca38eef2e2361219a85a5e2502f8617e2
Antonio <antoniomlf@protonmail.com>
2024-12-06 10:33:48 +0100

    Add ranges for W

--- latticefold/benches/single_operations.rs
+++ latticefold/benches/single_operations.rs
@@ -1,59 +1,90 @@
 #![allow(incomplete_features)]
 use ark_std::{test_rng, time::Duration, UniformRand};
-use criterion::{
-    criterion_group, criterion_main, AxisScale, Criterion,
-    PlotConfiguration,
-};
+use criterion::{criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion, PlotConfiguration};
 use cyclotomic_rings::rings::{GoldilocksRingNTT, SuitableRing};
-
 use std::fmt::Debug;
-use latticefold::arith::{Instance, Witness, CCCS, CCS};
-use latticefold::arith::r1cs::{get_test_r1cs, get_test_z_split};
+use latticefold::arith::{Arith, Instance, Witness, CCCS, CCS};
+use latticefold::arith::r1cs::{get_test_dummy_r1cs, get_test_dummy_z_split};
 use latticefold::commitment::AjtaiCommitmentScheme;
 use latticefold::decomposition_parameters::DecompositionParams;
 use latticefold::nifs::error::LinearizationError;
 use latticefold::utils::mle_helpers::{calculate_Mz_mles};
-use ark_std::rand::Rng;
 use lattirust_ring::Ring;
 use latticefold::nifs::linearization::utils::prepare_lin_sumcheck_polynomial;
 
+pub fn get_test_dummy_ccs<R: Ring, const X_LEN: usize, const WIT_LEN: usize, const W: usize>(
+    rows_size: usize,
+    l: usize,
+) -> CCS<R> {
+    let r1cs = get_test_dummy_r1cs::<R, X_LEN, WIT_LEN>(rows_size);
+    CCS::<R>::from_r1cs_padded(r1cs, W, l)
+}
+
+pub fn wit_and_ccs_gen<
+    const X_LEN: usize,
+    const C: usize, // rows
+    const WIT_LEN: usize,
+    const W: usize, // columns
+    P: DecompositionParams,
+    R: Clone + UniformRand + Debug + SuitableRing,
+>(
+    r1cs_rows: usize,
+) -> (
+    CCCS<C, R>,
+    Witness<R>,
+    CCS<R>,
+    AjtaiCommitmentScheme<C, W, R>,
+) {
+    let mut rng = test_rng();
+
+    let new_r1cs_rows = if P::L == 1 && (WIT_LEN > 0 && (WIT_LEN & (WIT_LEN - 1)) == 0) {
+        r1cs_rows - 2
+    } else {
+        r1cs_rows // This makes a square matrix but is too much memory
+    };
+    let ccs: CCS<R> = get_test_dummy_ccs::<R, X_LEN, WIT_LEN, W>(new_r1cs_rows, P::L);
+    let (one, x_ccs, w_ccs) = get_test_dummy_z_split::<R, X_LEN, WIT_LEN>();
+    let mut z = vec![one];
+    z.extend(&x_ccs);
+    z.extend(&w_ccs);
+    ccs.check_relation(&z).expect("R1CS invalid!");
+
+    let scheme: AjtaiCommitmentScheme<C, W, R> = AjtaiCommitmentScheme::rand(&mut rng);
+    let wit: Witness<R> = Witness::from_w_ccs::<P>(w_ccs);
+
+    let cm_i: CCCS<C, R> = CCCS {
+        cm: wit.commit::<C, W, P>(&scheme).unwrap(),
+        x_ccs,
+    };
+
+    (cm_i, wit, ccs, scheme)
+}
 
 #[derive(Clone)]
 pub struct GoldilocksDP;
 impl DecompositionParams for GoldilocksDP {
     const B: u128 = 1 << 15;
-    const L: usize = 5;
+    const L: usize = 4;
     const B_SMALL: usize = 2;
     const K: usize = 15;
-}
-pub fn get_test_ccs<R: Ring>(w: usize, l: usize) -> CCS<R> {
-    let r1cs = get_test_r1cs::<R>();
-    CCS::<R>::from_r1cs_padded(r1cs, w, l)
 }
 
 fn setup_test_environment<
     RqNTT: SuitableRing,
     DP: DecompositionParams,
     const C: usize,
-    const W: usize
->(
-    input: Option<usize>,
-) -> (
+    const W: usize,
+    const WIT_LEN: usize
+>() -> (
     Witness<RqNTT>,
     CCCS<C, RqNTT>,
     CCS<RqNTT>,
     AjtaiCommitmentScheme<C, W, RqNTT>,
 ) {
-    let ccs = get_test_ccs::<RqNTT>(W, DP::L);
     let mut rng = test_rng();
-    let (_, x_ccs, w_ccs) = get_test_z_split::<RqNTT>(input.unwrap_or(rng.gen_range(0..64)));
     let scheme = AjtaiCommitmentScheme::rand(&mut rng);
-
-    let wit = Witness::from_w_ccs::<DP>(w_ccs);
-    let cm_i = CCCS {
-        cm: wit.commit::<C, W, DP>(&scheme).unwrap(),
-        x_ccs,
-    };
+    let r1cs_rows = 1 + WIT_LEN + 1;
+    let (cm_i, wit, ccs, _) = wit_and_ccs_gen::<1, C, WIT_LEN, W, DP, RqNTT>(r1cs_rows);
 
     (wit, cm_i, ccs, scheme)
 }
@@ -103,13 +134,14 @@ fn linearization_operations<
     R: Clone + UniformRand + Debug + SuitableRing,
     DP: DecompositionParams,
     const C: usize,
-    const W: usize
+    const W: usize,
+    const WIT_LEN: usize
 >(
     group: &mut criterion::BenchmarkGroup<criterion::measurement::WallTime>,
 ) {
     let mut rng = test_rng();
     let (wit, cm_i, ccs, _) =
-        setup_test_environment::<R, DP, C, W>(None);
+        setup_test_environment::<R, DP, C, W, WIT_LEN>();
     let z_ccs = cm_i.get_z_vector(&wit.w_ccs);
 
     // Prepare MLEs
@@ -118,7 +150,7 @@ fn linearization_operations<
 
     // MZ mles
     group.bench_with_input(
-        "Evaluate Mz_MLEs",
+        BenchmarkId::new("Evaluate Mz_MLEs", format!("C= {}, W= {}", C, W)),
         &(ccs.clone(), z_ccs.clone()),
         |bench, (ccs, z_ccs)| {
             bench.iter(|| {
@@ -129,7 +161,7 @@ fn linearization_operations<
 
     // Prepare the main linearization polynomial.
     group.bench_with_input(
-        "Prepare Sumcheck Poly",
+        BenchmarkId::new("Prepare Sumcheck Poly", format!("C= {}, W= {}", C, W)),
         &(ccs, mz_mles, beta_s),
         |bench, (ccs, mz_mles, beta_s)| {
             bench.iter(|| {
@@ -150,12 +182,18 @@ fn single_operation_benchmarks(c: &mut Criterion) {
     // Individual operations
     single_op_benchmark::<GoldilocksRingNTT>(&mut group);
 
-    // Linearization (W, L)
-    // W = 2^9-16
-    // W = WITLEN * L
-    linearization_operations::<GoldilocksRingNTT, GoldilocksDP, 4, {4 * GoldilocksDP::L}>(&mut group);
-
-
+    // Linearization
+    // Please note that C is not used until decomposition.
+    // The only parameter that we are interested on varying for linearization is W (as it already includes WIT_LEN and DP::L)
+    // We explore parameters in the range  W = 2^9-2^16
+    linearization_operations::<GoldilocksRingNTT, GoldilocksDP, 4, 512, 128>(&mut group);
+    linearization_operations::<GoldilocksRingNTT, GoldilocksDP, 4, 1024, 256>(&mut group);
+    linearization_operations::<GoldilocksRingNTT, GoldilocksDP, 4, 2048, 512>(&mut group);
+    linearization_operations::<GoldilocksRingNTT, GoldilocksDP, 4, 4096, 1024>(&mut group);
+    linearization_operations::<GoldilocksRingNTT, GoldilocksDP, 4, 8192, 2048>(&mut group);
+    linearization_operations::<GoldilocksRingNTT, GoldilocksDP, 4, 16384, 4096>(&mut group);
+    linearization_operations::<GoldilocksRingNTT, GoldilocksDP, 4, 32768, 8192>(&mut group);
+    linearization_operations::<GoldilocksRingNTT, GoldilocksDP, 4, 65536, 16384>(&mut group);
 }
 
 pub fn benchmarks_main(c: &mut Criterion) {
