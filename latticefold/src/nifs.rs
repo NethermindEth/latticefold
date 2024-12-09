45e15b1dac8d35bce39d85559b1d81c2ba8f31ab
Marko Cupic <cupic_marko@outlook.com>
2024-12-09 17:05:09 +0100

    Implement Goldilocks example

--- latticefold/src/nifs.rs
+++ latticefold/src/nifs.rs
@@ -1,3 +1,4 @@
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 use ark_std::marker::PhantomData;
 
 use cyclotomic_rings::rings::SuitableRing;
@@ -27,7 +28,7 @@ mod tests;
 
 /// `C` is the length of Ajtai commitment vectors.
 /// `NTT` is a cyclotomic ring in the NTT form.
-#[derive(Clone)]
+#[derive(Clone, CanonicalSerialize, CanonicalDeserialize)]
 pub struct LFProof<const C: usize, NTT: OverField> {
     pub linearization_proof: LinearizationProof<NTT>,
     pub decomposition_proof_l: DecompositionProof<C, NTT>,
