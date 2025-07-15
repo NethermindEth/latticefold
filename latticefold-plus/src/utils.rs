d716f176ce2a68703b8c58e7f22dc3272fe797e1
v0-e <xyz.vieira@gmail.com>
2025-07-15 10:00:27 +0100

    Cm protocol

--- latticefold-plus/src/utils.rs
+++ latticefold-plus/src/utils.rs
@@ -1,4 +1,9 @@
-use ark_std::{iter::once, log2, Zero};
+use ark_std::{
+    iter::once,
+    log2,
+    ops::{Mul, Sub},
+    One, Zero,
+};
 use latticefold::{
     transcript::Transcript,
     utils::sumcheck::{
@@ -50,13 +55,64 @@ where
     tau
 }
 
-pub fn to_bits(n: usize, num_bits: usize) -> Vec<u8> {
-    (0..num_bits).map(|i| ((n >> i) & 1) as u8).collect()
+/// Computes the tensor product of two flat vectors.
+///
+/// If `a` has length `m` and `b` has length `n`, the result is a new vector
+/// of length `m * n` containing the element-wise products.
+pub fn tensor_product<T>(a: &[T], b: &[T]) -> Vec<T>
+where
+    T: Clone + Mul<Output = T>,
+{
+    if a.is_empty() {
+        return b.to_vec();
+    }
+    if b.is_empty() {
+        return a.to_vec();
+    }
+
+    let mut result = Vec::with_capacity(a.len() * b.len());
+    for a_val in a {
+        for b_val in b {
+            result.push(a_val.clone() * b_val.clone());
+        }
+    }
+    result
 }
 
-pub fn from_bits(bits: &[u8]) -> usize {
-    bits.iter()
-        .rev()
-        .enumerate()
-        .fold(0, |acc, (i, &b)| acc | ((b as usize) << i))
+/// Computes the tensor operation on a vector `r`.
+///
+/// This corresponds to the `tensor(r)` function, defined as the sequential
+/// tensor product of `(1 - r_i, r_i)` for each element `r_i` in the input vector.
+pub fn tensor<T>(r: &[T]) -> Vec<T>
+where
+    T: Clone + One + Sub<Output = T> + Mul<Output = T>,
+{
+    let mut result = vec![T::one()];
+
+    for r_i in r {
+        let one = T::one();
+        let term = [one - r_i.clone(), r_i.clone()];
+        result = tensor_product(&result, &term);
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tensor_product() {
+        let a = vec![1, 2];
+        let b = vec![10, 20, 30];
+        let expected = vec![10, 20, 30, 20, 40, 60];
+        assert_eq!(tensor_product(&a, &b), expected);
+    }
+
+    #[test]
+    fn test_tensor() {
+        let r = vec![10, 2];
+        let expected = vec![-9 * -1, -9 * 2, 10 * -1, 10 * 2];
+        assert_eq!(tensor(&r), expected);
+    }
 }
