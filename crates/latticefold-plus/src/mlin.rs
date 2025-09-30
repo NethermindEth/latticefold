c7e07adff8ffce595993a65035ea2b8c4a7dd509
Vishal Tiwari (Believer) <fantasydark001@gmail.com>
2025-09-30 22:39:54 +0530

    Optimize cm_g aggregation: replace fold with in-place for-loop

--- crates/latticefold-plus/src/mlin.rs
+++ crates/latticefold-plus/src/mlin.rs
@@ -63,16 +63,13 @@ where
 
         let (com, proof) = cm.prove(M, transcript);
 
-        let cm_g = com
-            .x
-            .cm_g
-            .iter()
-            .fold(vec![R::zero(); self.params.kappa], |mut acc, cm| {
-                acc.iter_mut().zip(cm.iter()).for_each(|(acc_r, cm_r)| {
-                    *acc_r += cm_r;
-                });
-                acc
-            });
+      let mut cm_g = vec![R::zero(); self.params.kappa];
+for cm in &com.x.cm_g {
+    for (acc_r, cm_r) in cm_g.iter_mut().zip(cm.iter()) {
+        *acc_r += cm_r;
+    }
+}
+
 
         let nlin = com.x.vo[0].len();
         let vo = com
